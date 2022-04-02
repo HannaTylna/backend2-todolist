b5e26231f628009df1bb06799947fd9ce671d032
Hanna <hanna.tylna@gmail.com>
2022-04-02 14:50:23 +0200

    add registration

--- server/app.js
+++ server/app.js
@@ -1,11 +1,37 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { User } = require("./models/user");
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Success!");
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  const user = new User({ username, password });
+  const createUser = await user.save();
+  const token = createUserToken(createUser);
+
+  res.json({ user: user, token: token });
 });
+
+const createUserToken = user => {
+  const userId = user._id.toString();
+  return (token = jwt.sign(
+    { userId, username: user.username },
+    process.env.JWT_SECRET,
+    { expiresIn: "24 h", subject: userId }
+  ));
+};
+
+mongoose.connect(`mongodb://localhost/toDoList`);
 
 app.listen(PORT, () => {
   console.log(`Started Express server on port ${PORT}`);
