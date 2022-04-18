0538ed7f6922379a17e4d46231311c473982121d
Hanna <hanna.tylna@gmail.com>
2022-04-18 17:55:16 +0200

    change user routes

--- server/routers/user.js
+++ server/routers/user.js
@@ -1,57 +1,54 @@
 const { Router } = require("express");
 const { User } = require("../models/user");
-const jwt = require("jsonwebtoken");
+const { createUserToken } = require("../utils/auth");
 
 const router = Router();
 
-router.post("/", async (req, res) => {
-  const { username, password } = req.body;
-  const user = new User({ username, password });
-  const createUser = await user.save();
-  const token = createUserToken(createUser);
-
-  res.json({ user: user, token: token });
-});
-
-const createUserToken = user => {
-  const userId = user._id.toString();
-  return (token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "24 h",
-    subject: userId
-  }));
-};
-
 router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.login(username, password);
+  try {
+    const { username, password } = req.body.user;
+    const user = await User.login(username, password);
 
-  if (user) {
-    const token = createUserToken(user);
-    return res.json({ user: user, token: token });
-  } else {
-    res.sendStatus(401);
+    if (user) {
+      const token = createUserToken(user);
+      return res.json({ user: { token, username } });
+    } else {
+      res.sendStatus(401).json("Wrong credentials!");
+    }
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
-router.get("/:username", async (req, res) => {
-  const username = req.params.username;
-  const user = await User.findOne({ username: username });
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body.user;
+    const user = new User({ username, password });
+    const createdUser = await user.save();
+    const token = createUserToken(createdUser);
+
+    res.json({ user: { username: createdUser.username, token } });
+  } catch (error) {
+    if (error.code && error.code === 11000) {
+      res.status(400).json("User is already in the system");
+    } else {
+      res.status(500).json("Internal server error");
+    }
+  }
+});
+
+router.get("/", async (req, res) => {
+  const user = req.user;
+  const { userId } = user;
+  console.log(userId);
+  const databaseUser = await User.findOne({ _id: userId });
   if (user) {
-    res.json(user);
+    res.json({
+      user: { username: databaseUser.username, token: req.token }
+    });
   } else {
     res.sendStatus(400);
   }
-});
-
-router.get("/logout", async (req, res) => {
-  const authHeader = req.header("Authorization");
-  jwt.sign(authHeader, "", { expiresIn: 1 }, (logout, err) => {
-    if (logout) {
-      res.json("You have been logged out!");
-    } else {
-      res.sendStatus(404);
-    }
-  });
 });
 
 module.exports = router;
